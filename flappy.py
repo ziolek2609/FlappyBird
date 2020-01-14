4b2f8432932b2dc53342c6f0bb3dcee22e253e96
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2020-01-14 17:24:44 +0100

    Add files via upload

--- flappy.py
+++ flappy.py
@@ -515,32 +515,32 @@ def checkCrash(player, upperPipes, lowerPipes):
     player['w'] = IMAGES['player'][0].get_width()
     player['h'] = IMAGES['player'][0].get_height()
 
-    # if player crashes into ground
-    #if player['y'] + player['h'] >= BASEY - 1:
-    #    return [True, True]
-    
+    #if player crashes into ground
+    if player['y'] + player['h'] >= BASEY - 1:
+        return [True, True]
+    else:
 
-    playerRect = pygame.Rect(player['x'], player['y'],
-                    player['w'], player['h'])
-    pipeW = IMAGES['pipe'][0].get_width()
-    pipeH = IMAGES['pipe'][0].get_height()
+        playerRect = pygame.Rect(player['x'], player['y'],
+                        player['w'], player['h'])
+        pipeW = IMAGES['pipe'][0].get_width()
+        pipeH = IMAGES['pipe'][0].get_height()
 
-    for uPipe, lPipe in zip(upperPipes, lowerPipes):
-        # upper and lower pipe rects
-        uPipeRect = pygame.Rect(uPipe['x'], uPipe['y'], pipeW, pipeH)
-        lPipeRect = pygame.Rect(lPipe['x'], lPipe['y'], pipeW, pipeH)
+        for uPipe, lPipe in zip(upperPipes, lowerPipes):
+            # upper and lower pipe rects
+            uPipeRect = pygame.Rect(uPipe['x'], uPipe['y'], pipeW, pipeH)
+            lPipeRect = pygame.Rect(lPipe['x'], lPipe['y'], pipeW, pipeH)
 
-        # player and upper/lower pipe hitmasks
-        pHitMask = HITMASKS['player'][pi]
-        uHitmask = HITMASKS['pipe'][0]
-        lHitmask = HITMASKS['pipe'][1]
+            # player and upper/lower pipe hitmasks
+            pHitMask = HITMASKS['player'][pi]
+            uHitmask = HITMASKS['pipe'][0]
+            lHitmask = HITMASKS['pipe'][1]
 
-        # if bird collided with upipe or lpipe
-        uCollide = pixelCollision(playerRect, uPipeRect, pHitMask, uHitmask)
-        lCollide = pixelCollision(playerRect, lPipeRect, pHitMask, lHitmask)
+            # if bird collided with upipe or lpipe
+            uCollide = pixelCollision(playerRect, uPipeRect, pHitMask, uHitmask)
+            lCollide = pixelCollision(playerRect, lPipeRect, pHitMask, lHitmask)
 
-        if uCollide or lCollide:
-            return [True, False]
+            if uCollide or lCollide:
+                return [True, False]
 
     return [False, False]
 
